9064cddd9bf4dd0f0e1aca7a0b2d14f0f5e06217
Anurag Sonkar <anuragsonkar053@gmail.com>
2025-09-15 04:05:18 -0700

    url-

--- src/utils/baseurl.js
+++ src/utils/baseurl.js
@@ -1,6 +1,6 @@
 // Local
 // export const base_url = import.meta.env.VITE_BASE_URL
-// export const base_url = 'http://localhost:5029'
-export const base_url = 'https://propertify-0tk4.onrender.com'
+export const base_url = 'http://localhost:5029'
+// export const base_url = 'https://propertify-0tk4.onrender.com'
 
 // https://profo-dashboard.netlify.app/       -deployed link
